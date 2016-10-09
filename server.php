aa9a0c7ba4e340a25af9a766e407e28e0539c060
stefan22 <stefan.emailacct@gmail.com>
2016-10-09 14:03:29 +0100

    accessing github api data

--- server.php
+++ server.php
@@ -3,7 +3,7 @@
 	// receive all data passed with GET
 	$data = $_GET;
 	// get data inside of user_text
-	$user_text = $_GET["user_text"];
+	$user_text = $data["user_text"];
 	// do something with it like uppercasing
 	$response = strtoupper($user_text);
 	// send it back to the client
