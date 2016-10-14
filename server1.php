96b943bcac88db654638b643257a5dc933d61de1
stefan22 <stefan.emailacct@gmail.com>
2016-10-15 00:32:04 +0100

    update same file

--- server1.php
+++ server1.php
@@ -4,16 +4,18 @@
 	//$data = $_POST;
 	
 	// if you're not sure whether the client it's using GET or POST
-	// the use $_REQUEST	
+	// then use $_REQUEST	
 	$data = $_REQUEST;
+	
+	//$user_text = $_REQUEST["user_text"];
 	// get data inside of user_text
 	$user_text = $data["user_text"];
 
-	//$user_text = $_POST["user_text"];
 	// do something with it like uppercasing
 	$response = strtoupper($user_text);
 	// send it back to the client
+	// i get array and response back on console and network tab
+	print_r($data);
 	echo $response;
-
 
 ?>
